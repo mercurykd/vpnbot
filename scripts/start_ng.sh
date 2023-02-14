bdf32595a179961f40e7ce304ea50012581ce7eb
mercury <mercury.kd@gmail.com>
2023-02-14 16:42:21 +0400

    fix v2ray after change port ss

--- scripts/start_ng.sh
+++ scripts/start_ng.sh
@@ -1,3 +1,7 @@
 cat /ssh/key.pub > /root/.ssh/authorized_keys
 service ssh start
+sed "s/ss:[0-9]\+/ss:$SSPORT/" /nginx_default.conf > change_port
+cat change_port > /nginx_default.conf
+sed "s/ss:[0-9]\+/ss:$SSPORT/" /etc/nginx/nginx.conf > change_port
+cat change_port > /etc/nginx/nginx.conf
 nginx -g "daemon off;"
