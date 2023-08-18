8533ca79d7974a10c4d01f69997f3a5bd1f7d9a1
mercury <mercury.kd@gmail.com>
2023-08-18 18:53:37 +0400

    fix rout after reset wg

--- scripts/reset_wg.sh
+++ scripts/reset_wg.sh
@@ -4,7 +4,7 @@ echo "[Interface]" > /etc/wireguard/wg0.conf
 echo "PrivateKey = $PRIVATEKEY" >> /etc/wireguard/wg0.conf
 echo "Address = $1" >> /etc/wireguard/wg0.conf
 echo "ListenPort = $2" >> /etc/wireguard/wg0.conf
-echo "PostUp = iptables -A FORWARD -i %i -j ACCEPT; iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE" >> /etc/wireguard/wg0.conf
-echo "PostDown = iptables -D FORWARD -i %i -j ACCEPT; iptables -t nat -D POSTROUTING -o $INTERFACE -j MASQUERADE" >> /etc/wireguard/wg0.conf
+echo "PostUp = iptables -t nat -A POSTROUTING --destination 10.10.0.5 -j ACCEPT;iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE" >> /etc/wireguard/wg0.conf
+echo "PostDown = iptables -t nat -D POSTROUTING --destination 10.10.0.5 -j ACCEPT;iptables -t nat -D POSTROUTING -o $INTERFACE -j MASQUERADE" >> /etc/wireguard/wg0.conf
 wg-quick down wg0
 wg-quick up wg0
