2b67a2c3bf4c47b26b1e81967dc8603626ac2c3c
mercury <mercury.kd@gmail.com>
2024-02-06 20:52:39 +0400

    openconnect -> dns adguard

--- scripts/start_ad.sh
+++ scripts/start_ad.sh
@@ -1,4 +1,5 @@
 route add -net 10.0.1.0 netmask 255.255.255.0 gw wg
+route add -net 10.0.2.0 netmask 255.255.255.0 gw oc
 cat /ssh/key.pub > /root/.ssh/authorized_keys
 ssh-keygen -A
 exec /usr/sbin/sshd -D -e "$@" &
--- scripts/start_oc.sh
+++ scripts/start_oc.sh
@@ -1,9 +1,11 @@
+INTERFACE=$(route | grep '^default' | grep -o '[^ ]*$')
 cat /ssh/key.pub > /root/.ssh/authorized_keys
 ssh-keygen -A
 exec /usr/sbin/sshd -D -e "$@" &
 mkdir -p /dev/net
 mknod /dev/net/tun c 10 200
 chmod 600 /dev/net/tun
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING --destination 10.10.0.5 -j ACCEPT
+iptables -t nat -A POSTROUTING -o $INTERFACE -j MASQUERADE
 ocserv -c /etc/ocserv/ocserv.conf
 tail -f /dev/null
