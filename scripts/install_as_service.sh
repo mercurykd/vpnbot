11e34d8c4e1a303848a7af59e99666377c6a021d
mercury <mercury.kd@gmail.com>
2024-03-06 15:15:45 +0400

    unversary path for vpnbot.service

--- scripts/install_as_service.sh
+++ scripts/install_as_service.sh
@@ -1,3 +1,4 @@
-cp scripts/vpnbot.service /etc/systemd/system/vpnbot.service
+path=`pwd`
+sed "s|path|$path|g" "$path/scripts/vpnbot.service" > /etc/systemd/system/vpnbot.service
 systemctl daemon-reload
 systemctl enable vpnbot
