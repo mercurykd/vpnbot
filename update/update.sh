7d12e5c9727137dd9272c8a42c80c9c2341cd207
mercury <mercury.kd@gmail.com>
2024-03-21 01:36:11 +0400

    improve update script

--- update/update.sh
+++ update/update.sh
@@ -11,11 +11,12 @@ do
     then
         docker compose down --remove-orphans
         git reset --hard && git clean -fd
-        git pull > ./update/message
+        git fetch
         if [[ -n "$branch" ]]
         then
             git checkout -t origin/$branch || git checkout $branch
         fi
+        git pull > ./update/message
         IP=$(curl https://ipinfo.io/ip) VER=$(git describe --tags) docker compose up -d --force-recreate
         bash $pwd/update/update.sh &
         exit 0
