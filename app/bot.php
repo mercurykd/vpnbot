18a4ee39fb4c14045ebb23199a9386497931ecab
mercury <mercury.kd@gmail.com>
2024-10-23 23:59:32 +0400

    improve selfupdate

--- app/bot.php
+++ app/bot.php
@@ -1805,7 +1805,7 @@ class Bot
 
         $this->input['chat']        = $c['admin'][0];
         $this->input['message_id']  = $r['result']['message_id'];
-        $this->input['callback_id'] = $r['result']['message_id'];
+        $this->input['callback_id'] = false;
         if (empty($p['domain'])) {
             $this->addDomain(str_replace('.', '-', $this->ip) . '.nip.io', 1);
             $this->setSSL('letsencrypt');
