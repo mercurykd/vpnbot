62c4314bebc59a0ac25e4e556684cca1a0b2cc1c
mercury <mercury.kd@gmail.com>
2024-11-05 18:35:31 +0400

    docker installation

--- scripts/init.sh
+++ scripts/init.sh
@@ -10,17 +10,7 @@ apt install -y \
     iproute2 \
     xtables-addons-common \
     xtables-addons-dkms
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]')/gpg | gpg --dearmor --yes -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(lsb_release -is | tr '[:upper:]' '[:lower:]') \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt update
-apt install -y docker-ce docker-ce-cli containerd.io
-DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
-mkdir -p $DOCKER_CONFIG/cli-plugins
-curl -SL https://github.com/docker/compose/releases/download/v2.29.6/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
-chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
 git clone https://github.com/mercurykd/vpnbot.git
 cd ./vpnbot
 echo "<?php
