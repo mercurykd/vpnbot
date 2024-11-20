dbe3f290ea031cdbbe7213069223bd890da3159d
mercury <mercury.kd@gmail.com>
2024-11-20 15:46:03 +0400

    commands reset and backup

---
+++ app/backup.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/timezone.php';
+
+require __DIR__ . '/bot.php';
+require __DIR__ . '/config.php';
+require __DIR__ . '/i18n.php';
+if ($c['debug']) {
+    require __DIR__ . '/debug.php';
+}
+
+$bot = new Bot($c['key'], $i);
+echo $bot->export();
